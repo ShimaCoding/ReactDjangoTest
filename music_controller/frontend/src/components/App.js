6ac30901826a06e40796d43fe3ff7b5b3c0dee45
ShimaCoding <shimacoding@gmail.com>
2021-05-28 13:00:11 -0400

    React Router and components

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,17 +1,20 @@
-import React,{ Component } from "react";
+import React, { Component } from "react";
 import { render } from "react-dom";
+import HomePage from "./HomePage";
+
 
 export default class App extends Component {
-    constructor(props){
-        super(props);
-    }
-    render(){
-        return (
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
       <div>
-        <h1>Testing React Code</h1>
+        <HomePage />
+
       </div>
     );
-    }
+  }
 }
 const appDiv = document.getElementById("app");
-render(<App />, appDiv);
+render(<App name="Bob" />, appDiv);
